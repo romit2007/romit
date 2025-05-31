e72dc3c9e69929d9bb26dabf4f886f49e4d8f5f9
romit2007 <romitvaghela2007@gmail.com>
2025-05-31 05:51:42 -0700

    Add files via upload

---
+++ main (1).c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int M;
+    
+    printf("Enter marks");
+    scanf("%d",&M);
+    
+    if(M>=35)
+    {
+        printf ("pass");
+    }
+    else
+    {
+        printf("fail");
+    
+    }
+    
+    getch();
+}
+   
+    
+    
+    
+    
