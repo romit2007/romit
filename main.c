f8a68d3ba8ab42aee40722fad65baa8bc9b0523a
romit2007 <romitvaghela2007@gmail.com>
2025-05-31 05:41:45 -0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int R;
+   
+    printf("Enter age");
+    scanf("%d",&R);
+    
+    if(R>=18)
+    {
+        printf("\n Eligibale for vote");
+    }
+    else
+    {
+        printf("\n don't Eligibale for vote");
+    
+    }
+    
+    getch();
+}
+    
+    
+    
