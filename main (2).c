3220d8d95ca919aab66515aa1027b02780e9ad63
romit2007 <romitvaghela2007@gmail.com>
2025-05-31 05:55:16 -0700

    Add files via upload

---
+++ main (2).c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int T;
+    
+    printf("Enter number");
+    scanf("%d",&T);
+    
+    if(T>0)
+    {
+        printf ("positive");
+    }
+    else
+    {
+        printf("Negative");
+    
+    }
+    
+    getch();
+}
+   
+    
+    
+    
+    
