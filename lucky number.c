635e26dc766ae901ce698782559f5a6e01597570
romit2007 <romitvaghela2007@gmail.com>
2025-06-05 17:54:14 +0530

    Add files via upload

---
+++ lucky number.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int usernum;
+    int luckynum=5;
+    
+    printf("Enter number between 1 to 10");
+    scanf("%d",&usernum);
+    
+    printf("\n your luckynum is %d",luckynum);
+    luckynum=usernum;
+    printf("\n your luckynum is %d",luckynum);
+    
+    
+    
+    
+    
+    getch();
+}
