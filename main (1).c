93f65243998a1b1b1f39079e6bbd8f73442bade3
romit2007 <romitvaghela2007@gmail.com>
2025-06-06 14:11:12 +0530

    Add files via upload

--- main (1).c
+++ main (1).c
@@ -2,25 +2,47 @@
 #include<conio.h>
 void main()
 {
-    int M;
+    int num1,num2,num3;
     
-    printf("Enter marks");
-    scanf("%d",&M);
+    printf("Enter num1: ");
+    scanf("%d",&num1);
     
-    if(M>=35)
-    {
-        printf ("pass");
-    }
-    else
-    {
-        printf("fail");
+    printf("Enter num2: ");
+    scanf("%d",&num2);
     
-    }
+     printf("Enter num3: ");
+     scanf("%d",&num3);
+     
+     if(num1>num2)
+     {
+         if(num1>num3)
+         {
+             printf("num1 is bigger");
+         }
+         else
+         {
+             printf("num3 is bigger");
+         }
+     }
+     else
+     {
+         
+         //printf("num2 is bigger");
+         
+          if(num2>num3)
+         {
+              printf("num2 is bigger");
+         }
+         else
+         {
+                printf("num3 is bigger"); 
+         }     
+     }
+     
+    
+    
     
     getch();
 }
-   
     
-    
-    
-    
+                    
