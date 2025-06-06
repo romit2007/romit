1b52e70d5459754b5cf4bab74e0228ca299a8af2
romit2007 <romitvaghela2007@gmail.com>
2025-06-06 15:13:58 +0530

    Add files via upload

---
+++ number sweping.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int A,B,temp;
+    
+    printf("Enter two numbers: ");
+    scanf("%d %d",&A,&B);
+    
+    printf("Before Swapping: A=%d,B=%d",A,B);
+    
+    temp=A;
+    A=B;
+    B=temp;
+    
+    printf("After swapping: A = %d, B = %d", A, B);
+    
+    
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+
+    
+    getch();
+}
+
