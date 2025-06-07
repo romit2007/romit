75ebef2257fb91f90b1b0dd61135e1f53f9e82b6
romit2007 <romitvaghela2007@gmail.com>
2025-06-07 05:38:42 -0700

    Add files via upload

---
+++ 7-6-2025 task/check number mulpile by 7.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int num;
+    
+    printf("Enter num: ");
+    scanf("%d",&num);
+    
+    if(num%7==0)
+    {
+        printf("multipale by 7 ");
+    }
+    else
+    {
+        printf("Not multipale by 7");
+    }
+    
+    
+    
+    
+    getch();
+}
