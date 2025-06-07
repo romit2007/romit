8de5345ed3d3e78b41da7f8f7dac555c131ca7f3
romit2007 <romitvaghela2007@gmail.com>
2025-06-07 00:16:03 -0700

    Add files via upload

---
+++ 7-6-2025 task/Check number possitive,negative,zero.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int num;
+    
+    printf("Enter num : ");
+    scanf("%d",&num);
+    
+    if(num>0)
+    {
+        printf("possitive");
+    }
+    else if(num<0)
+    {
+        printf("Negative");
+    }
+    else
+    {
+        printf("Zero");
+    }
+    
+    
+    
+    getch();
+}
