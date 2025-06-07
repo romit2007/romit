1449c5cc70cc37952d461c2f5db620b440c9aa84
romit2007 <romitvaghela2007@gmail.com>
2025-06-07 00:08:04 -0700

    Add files via upload

---
+++ 7-6-2025 task/Find odd even number.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int num;
+    
+    printf("Enter num : ");
+    scanf("%d",&num);
+    
+    if(num%2)
+    {
+        printf("odd");
+    }
+    else
+    {
+        printf("Even");
+    }
+    
+    
+    
+    
+    getch();
+}
