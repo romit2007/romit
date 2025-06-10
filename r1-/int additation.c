2a9bf04677b48b86c8d4e374e1e056ce7ed823cd
romit2007 <romitvaghela2007@gmail.com>
2025-06-10 17:33:40 +0530

    Add files via upload

---
+++ r1-/int additation.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a = 3;
+    int b = 9;
+    
+    a+=b;
+    printf("%d",a);
+    
+    b-=a;
+    printf("%d",b);
+    
+    
+    
+  getch();   
+}
