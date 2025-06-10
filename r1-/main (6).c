30a9bb72e9a44413d2f24f17d655528c65f5cd43
romit2007 <romitvaghela2007@gmail.com>
2025-06-09 23:47:32 -0700

    Add files via upload

---
+++ r1-/main (6).c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ 
+   float num;
+   
+   printf("\n Enter number: ");
+   scanf("%f",&num);
+   printf("\n your number is %f",num);
+    
+    
+    getch();
+}
