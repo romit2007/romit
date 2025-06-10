1a245c6d4c796e5a8de84ff1e8810b5ccc3b0b01
romit2007 <romitvaghela2007@gmail.com>
2025-06-10 18:03:01 +0530

    Add files via upload

---
+++ r1-/while loop increse.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   int i=1;
+   
+    
+    while(i<=100)
+    {
+        printf("\n %d",i);
+        i++;
+    }
+   
+    
+ 
+    
+    
+    return 0;
+}
