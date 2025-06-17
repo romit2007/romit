35e6837233fbc0c9db01079a57ecb33bcd476e67
romit2007 <romitvaghela2007@gmail.com>
2025-06-17 11:37:46 +0530

    Add files via upload

---
+++ Table.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+        int num,ans=0,i;
+        
+        printf("Enter any number");
+        scanf("%d",&num);
+        
+        printf("Table of given number is: ");
+        for(i=1;i<=10;i++)
+
+        {
+            ans=num*i;
+            printf("\n %d * %d = %d",num,i,ans);
+        }
+    
+
+
+    getch();
+}
