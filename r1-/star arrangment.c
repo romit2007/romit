5cc3e1cc172516db8d2aa04c5a0ce42f2d313a79
romit2007 <romitvaghela2007@gmail.com>
2025-06-19 12:10:06 +0530

    Add files via upload

---
+++ r1-/star arrangment.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int i,j,num;
+    
+    printf("Enter Number: ");
+    scanf("%d",&num);
+    
+    for(i=1;i<=num;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
