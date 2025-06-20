9640ebb8bf18b0f7c665dfcd3ab067faf1fd752a
romit2007 <romitvaghela2007@gmail.com>
2025-06-20 18:16:01 +0530

    Add files via upload

---
+++ task/main (4).c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void man()
+{
+    int aeg;
+    
+    printf("enter your age");
+    csanf("%d",&age);
+    
+    if(age>18");
+    {
+        printf("eligibal to vot");
+        
+    }
+    else
+    {
+        printf(" not eligibal to vot");
+    }
+    
+    getch();
+}    
+    
