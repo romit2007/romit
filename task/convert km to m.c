49a375806c5b1bbe00eb3d71af37d57121b3dd4f
romit2007 <romitvaghela2007@gmail.com>
2025-06-20 14:01:25 +0530

    Add files via upload

---
+++ task/convert km to m.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    float kilometer,meter;
+    
+    printf("Enter kilometer: ");
+    scanf("%f",&kilometer);
+    
+    meter=kilometer*1000;
+    
+    printf("\n kilometer is convert to meter %2f\n",meter);
+    
+
+    
+    
+    
+    getch();
+}
