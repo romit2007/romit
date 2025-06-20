b235d5379b5a387fd3b8c63976b282340f046e0f
romit2007 <romitvaghela2007@gmail.com>
2025-06-20 11:00:32 +0530

    Add files via upload

---
+++ task/yearly salary.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   float ammount,salary;
+   
+   printf("Enter ammount: ");
+   scanf("%f",&ammount);
+   
+   
+   
+   salary=ammount*12;
+   
+   printf("year salary : $%.2f\n", salary);
+   
+   
+   
+   
+   getch();
+}
