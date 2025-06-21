4016b559fb019996f9e8cbdf3f67a99cf68b0977
romit2007 <romitvaghela2007@gmail.com>
2025-06-21 14:30:25 +0530

    Add files via upload

---
+++ r1-/perchantage by array function.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    
+    int marks[7];
+    int i,sum=0,avg;
+
+    for(i=0;i<7;i++)
+    {
+        printf("\n Enter Your Marks");    
+        scanf("%d",&marks[i]);
+    }
+    
+     for(i=0;i<7;i++)
+    {
+         sum+= marks[i];
+     }
+      avg = sum/7;
+     printf("\n Your Percentage is %d",avg);    
+
+   
+    
+    
+    
+    
+    
+    getch();
+}
+
