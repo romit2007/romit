b177baca656bbdc5708e1ad361e90fa352994c6f
romit2007 <romitvaghela2007@gmail.com>
2025-06-21 12:16:44 +0530

    Add files via upload

---
+++ r1-/print total 5 mark from array program.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   
+    int marks [5],i;
+    
+    
+    marks[0]=10;
+    marks[1]=20;
+    marks[2]=30;
+    marks[3]=40;
+    marks[4]=50;
+    
+    
+    
+    
+    
+    
+    
+    
+    for(i=0;i<5;i++)
+    {
+        printf("\n %d",marks[i]);
+    }
+
+    
+    
+    
+    
+    getch();
+}
