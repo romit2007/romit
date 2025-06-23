a96d5b7b080ae7e29f548b546bb4ffd616d3c629
romit2007 <romitvaghela2007@gmail.com>
2025-06-23 18:35:36 +0530

    Add files via upload

---
+++ task/get name of subjects by userinput calculate perchantage.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int marks[7];
+    int i,sum=0;
+    char subjects[100];
+    float avg;
+    
+    for(i=0;i<7;i++)
+    {
+        printf("\n Enter name of subjects");
+        scanf("%s",&subjects[i]);
+    }
+
+    for(i=0;i<7;i++)
+    {
+        printf("\n Enter Your Marks");    
+        scanf("%d",&marks[i]);
+    }
+    
+     for(i=0;i<7;i++)
+    {
+         sum+= marks[i];
+     }
+      avg = sum/7;
+     printf("\n Your Percentage is %f",avg); 
+    
+
+   
+    
+    
+    
+    
+    getch();
+}
+
