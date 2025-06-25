5460c576c52473f87c8d603fd22dacd599a05134
romit2007 <romitvaghela2007@gmail.com>
2025-06-25 19:23:42 +0530

    Add files via upload

---
+++ task/pyramid pattern.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() 
+{
+    int i, j, space, rows;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    for(i = 1; i <= rows; i++)
+    {
+        // Print leading spaces
+        for(space = 1; space <= rows - i; space++)
+        {
+            printf(" ");
+        }
+
+          for(j = 1; j <= (2 * i - 1); j++) 
+        {
+            printf("*");
+        }
+
+        
+        printf("\n");
+    }
+
+    return 0;
+}
