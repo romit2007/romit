a448a4fdb158cfa36a6f2292443a38ae7835770f
romit2007 <romitvaghela2007@gmail.com>
2025-07-01 18:03:00 +0530

    Add files via upload

---
+++ c language/odd or even.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int num;
+    
+    printf("Enter num: ");
+    scanf("%d",&num);
+    
+    
+    
+    if(num%2==0)
+    {
+        printf("Even");
+    }
+    else
+    {
+        printf("odd");
+    }
+    
+
+    
+    
+    getch();
+}
+    
+    
+    
+    
