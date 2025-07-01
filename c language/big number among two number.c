dadfbcf3741166e7c4dca5abded17124c0bcb73d
romit2007 <romitvaghela2007@gmail.com>
2025-07-01 17:59:19 +0530

    Add files via upload

---
+++ c language/big number among two number.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int num1,num2;
+    
+    printf("Enter num1: ");
+    scanf("%d",&num1);
+    
+    printf("Enter num2: ");
+    scanf("%d",&num2);
+    
+    if(num1>=num2)
+    {
+        printf("num1 is big");
+    }
+    else
+    {
+        printf("num2 is big");
+    }
+    
+
+    
+    
+    getch();
+}
+    
+    
+    
+    
