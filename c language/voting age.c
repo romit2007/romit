f525efd5141ea063039b3af898f5819babe0b6a8
romit2007 <romitvaghela2007@gmail.com>
2025-07-01 18:09:27 +0530

    Add files via upload

---
+++ c language/voting age.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int age;
+    
+    printf("Enter age: ");
+    scanf("%d",&age);
+    
+    if(age>=18)
+    {
+        printf("Eligible to vote");
+    }
+    else
+    {
+        printf("Not Eligible to vote");
+    }
+    
+    
+    
+    
+
+    
+    
+    getch();
+}
+    
+    
+    
+    
