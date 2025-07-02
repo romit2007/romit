24c967fbe62b353884a5f243c9924fa2e86b11d3
romit2007 <romitvaghela2007@gmail.com>
2025-07-02 18:08:38 +0530

    Add files via upload

---
+++ c language/break number in 9.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i=10;
+    
+    do
+    {
+        printf("\n %d",i);
+        i++;
+        
+    }
+    while(i<=9);
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
