da52edb20b02f799cc6c73f371522bec5820094b
romit2007 <romitvaghela2007@gmail.com>
2025-07-02 17:47:18 +0530

    Add files via upload

---
+++ c language/calculation in shorthand opreater.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a = 15;
+    int b = 30;
+    
+    a+=b;
+    printf("\n %d",a);
+    
+    b-=a;
+    printf("\n %d",b);
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
