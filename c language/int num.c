a306f5e1df8f61c65df9569103636d6416984979
romit2007 <romitvaghela2007@gmail.com>
2025-07-02 17:21:21 +0530

    Add files via upload

---
+++ c language/int num.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int num;
+    
+    printf("Enter number: ");
+    scanf("%d",&num);
+    printf("\nYour number is %d",num);
+    
+    
+    
+    
+    
+    
+    getch();
+}
