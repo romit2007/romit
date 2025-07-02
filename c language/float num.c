94cfd1fd435022ed325a5a4c582b3dfb546724ca
romit2007 <romitvaghela2007@gmail.com>
2025-07-02 17:18:36 +0530

    Add files via upload

---
+++ c language/float num.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    float num;
+    
+    printf("Enter number: ");
+    scanf("%f",&num);
+    printf("\n Your number is %f",num);
+    
+    
+    
+    
+    
+    
+    getch();
+}
