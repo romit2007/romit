fe7a508f81aca7a9a9f856301db3af62f3f60ce5
romit2007 <romitvaghela2007@gmail.com>
2025-07-02 18:05:22 +0530

    Add files via upload

---
+++ c language/ascending order in do while loop.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i=1;
+    
+    do
+    {
+        printf("\n %d",i);
+        i++;
+        
+    }
+    while(i<=10);
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
