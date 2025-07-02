81cdce52888dd9f8358fcbd469bd7068df93a4cf
romit2007 <romitvaghela2007@gmail.com>
2025-07-02 18:16:15 +0530

    Add files via upload

---
+++ c language/break number 5.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i;
+    
+    for(i=1;i<=10;i++)
+    {
+       if(i==5)
+       {
+           
+           //break;
+           continue;
+           
+       }
+       printf("\n %d",i);
+       
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
