27e7e12d76a6c8b7bba30ecce216f47d60b00fb0
romit2007 <romitvaghela2007@gmail.com>
2025-07-04 16:18:21 +0530

    Add files via upload

---
+++ c language/number is multiple by 7.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    
+   int num;
+   
+   printf("Enter number: ");
+   scanf("%d",&num);
+   
+   if(num%7==0)
+   {
+       printf("number is multiple by 7");
+   }
+   else
+   {
+       printf("number isn't multiple by 7");
+   }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    getch();
+}
