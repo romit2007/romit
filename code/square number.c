a84d32ee697254e52ae2d62a716e6757f1aefd83
romit2007 <romitvaghela2007@gmail.com>
2025-07-05 18:21:48 +0530

    Add files via upload

---
+++ code/square number.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int num,square;
+    
+    printf("Enter number: ");
+    scanf("%d",&num);
+    
+    square=num*num;
+    printf("\n %d ",square);
+
+
+    
+    
+    getch();
+}
+
+    
+    
+    
+    
+    
