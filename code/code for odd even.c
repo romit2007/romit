aff5ed5c9fae9df3eaccba4c2538d002b603a63c
romit2007 <romitvaghela2007@gmail.com>
2025-07-05 17:58:49 +0530

    Add files via upload

---
+++ code/code for odd even.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int num;
+    
+    printf("Enter number: ");
+    scanf("%d",&num);
+    
+    if(num%2==0)
+    {
+        printf("even");
+    }
+    else
+    {
+        printf("odd");
+    }
+    
+    
+    
+    
+    getch();
+}
+
+    
+    
+    
+    
+    
