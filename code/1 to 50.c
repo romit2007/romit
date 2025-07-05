8b618030606ed1f9613ff885ca4b82590362e849
romit2007 <romitvaghela2007@gmail.com>
2025-07-05 18:02:00 +0530

    Add files via upload

---
+++ code/1 to 50.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i;
+    
+    for(i=1;i<=50;i++)
+    {
+        printf("\n %d",i);
+    }
+    
+    
+    
+    getch();
+}
+
+    
+    
+    
+    
+    
