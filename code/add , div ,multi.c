649a755b04295847f5e31691cf09a7478dc42b0b
romit2007 <romitvaghela2007@gmail.com>
2025-07-05 18:32:50 +0530

    Add files via upload

---
+++ code/add , div ,multi.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int num1,num2;
+    int sum,multi,divison,modulus;
+    
+    printf("enter num1: ");
+    scanf("%d",&num1);
+    
+    printf("enter num2: ");
+    scanf("%d",&num2);
+    
+    sum=num1+num2;
+    printf("\n %d + %d = %d",num1,num2,sum);
+    
+    multi=num1*num2;
+    printf("\n %d * %d = %d",num1,num2,multi);
+    
+    divison=num1/num2;
+    printf("\n %d / %d = %d",num1,num2,divison);
+    
+    
+
+
+
+    
+    
+    getch();
+}
+
+    
+    
+    
+    
+    
