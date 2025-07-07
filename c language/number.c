f30090acd93a7ad108208dfcbc35f9368f1e7501
romit2007 <romitvaghela2007@gmail.com>
2025-07-07 16:48:27 +0530

    Add files via upload

---
+++ c language/number.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i,j,num;
+    
+    printf("Enter number");
+    scanf("%d",&num);
+
+    for(i=1;i<=num;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%d",i);
+        }
+        printf("\n");
+    }
+    
+    
+    
+    
+    return 0;
+}
+
+
+
+
