0346ec5f9173d95c17c1de2dace234fc90037378
romit2007 <romitvaghela2007@gmail.com>
2025-07-09 16:09:56 +0530

    Add files via upload

---
+++ c language/star.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i,j,num;
+    
+    printf("Enter number: ");
+    scanf("%d",&num);
+    
+    for(i=1;i<=num;i++)
+    {
+       for(j=1;j<=i;j++)
+        {
+           printf("*"); 
+        }
+        printf("\n");
+    }
+    
+}
