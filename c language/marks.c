af4acb8f3d36d82aed95d619b9ab338acd396ca1
romit2007 <romitvaghela2007@gmail.com>
2025-07-09 17:19:19 +0530

    Add files via upload

---
+++ c language/marks.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int marks[3],i;
+    
+    marks[0]=67;
+    marks[1]=60;
+    marks[2]=75;
+    
+    for(i=0;i<3;i++)
+    {
+        printf("\n %d",marks[i]);
+    }
+    
+}
