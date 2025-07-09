5e2505a2243868d06fb8a403d1583bbcc0e99f92
romit2007 <romitvaghela2007@gmail.com>
2025-07-09 18:21:14 +0530

    Add files via upload

---
+++ c language/array by perchantage.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    
+    int marks[7];
+    int i,sum=0,avg;
+    
+    for(i=0;i<7;i++)
+    {
+        printf("\n Enter mark: ");
+        scanf("%d",&marks[i]);
+    }
+    
+    for(i=0;i<7;i++)
+    {
+        sum+=marks[i];
+    }
+    avg=sum/7;
+    printf("\n perchantage is %d",avg);
+    
+    
+    
+    getch();
+}
