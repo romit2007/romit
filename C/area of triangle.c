cfbe650c1c90d3a795195ced895c044458e5e97b
romit2007 <romitvaghela2007@gmail.com>
2025-07-11 16:24:23 +0530

    Add files via upload

---
+++ C/area of triangle.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    float a,b;
+    float ans;
+    
+    printf("Enter a: ");
+    scanf("%f",&a);
+    printf("Enter b: ");
+    scanf("%f",&b);
+    ans=0.5*a*b;
+    printf("Area of triangle is %f",ans);
+    
+}
