6797f430defc4a62a8e079f9a7d6d32142b19f46
romit2007 <romitvaghela2007@gmail.com>
2025-07-12 09:27:41 +0530

    Add files via upload

---
+++ C/task1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int marks[]={33,34,45,44,55,65,45,76,87,45};
+    int i;
+    
+    for(i=0;i<10;i++)
+    {
+        printf("\n %d",marks[i]);
+    }
+}
