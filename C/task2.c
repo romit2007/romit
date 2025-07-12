50317b6e936280a41eaf36583f48d0af0eef9ec0
romit2007 <romitvaghela2007@gmail.com>
2025-07-12 09:32:09 +0530

    Add files via upload

---
+++ C/task2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i,j;
+    int marks[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+    
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("\n [%d] [%d] = %d",i,j,marks[i][j]);
+        }
+        printf("\n");
+    }
+}
