b86f5dd727d4dcc3d204ddefacb020cc0b86d1e7
romit2007 <romitvaghela2007@gmail.com>
2025-07-12 09:40:31 +0530

    Add files via upload

---
+++ C/average.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int marks[7];
+    int i,sum=0,avg;
+    
+    for(i=0;i<7;i++)
+    {
+        printf("\n Enter marks");
+        scanf("%d",&marks[i]);
+    }
+    
+    for(i=0;i<5;i++)
+    {
+         sum+= marks[i];
+     }
+      avg = sum/7;
+     printf("\n Your Percentage is %d",avg);    
+    
+    
+    
+    
+    
+    
+    getch();
+}
+    
