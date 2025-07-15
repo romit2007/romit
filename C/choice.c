e18f1d17e4b4761e97076e6f85a340a6f1512b2b
romit2007 <romitvaghela2007@gmail.com>
2025-07-15 17:58:25 +0530

    Add files via upload

---
+++ C/choice.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int num1,num2,num;
+    int add,multi,sub,divison;
+    
+    printf("Enter num1");
+    scanf("%d",&num1);
+    
+    printf("Enter num2");
+    scanf("%d",&num2);
+    
+    printf("\n press1 for add \n press2 for sub \n press3 for multi \n press4 for divison");
+    printf("\n Enter number");
+    scanf("%d",&num);
+    
+    switch(num)
+    {
+        case 1 : add=num1+num2;
+                 printf("\n %d + %d = %d",num1,num2,add);
+                 scanf("%d %d %d",&num1,&num2,&add);
+                 break;
+                 
+        case 2 : sub=num1-num2;
+                 printf("\n %d - %d = %d",num1,num2,sub);
+                 scanf("%d %d %d",&num1,&num2,&sub);
+                 break;
+        
+        case 3 : multi=num1*num2;
+                 printf("\n %d * %d = %d",num1,num2,multi);
+                 scanf("%d %d %d",&num1,&num2,&multi);
+                 break;
+                 
+        case 4 : divison=num1/num2;
+                 printf("\n %d / %d = %d",num1,num2,divison);
+                 scanf("%d %d %d",&num1,&num2,&divison);
+                 break;
+                 
+        default: printf("Invalid choice");
+                 break;
+    }
+    
+    
+
+    
+}
