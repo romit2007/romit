fc3c7b4a97cdb5543c07f955604dbbfcbc6e9900
romit2007 <romitvaghela2007@gmail.com>
2025-07-15 17:02:45 +0530

    Add files via upload

---
+++ C/pattern3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i,j;
+    int num;
+    
+    printf("Enter number");
+    scanf("%d",&num);
+    
+    for(i=1;i<=num;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+    
+}
