a55e66c347214f1735be6e6476803b812905d3c8
romit2007 <romitvaghela2007@gmail.com>
2025-07-18 16:49:29 +0530

    Add files via upload

---
+++ task/task4.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int marks[]={10,20,30,40,50,60,70,80,90,100};
+    int i;
+    
+    
+    for(i=0;i<10;i++)
+    {
+        printf("\n %d",marks[i]);
+    }
+    
+    
+    getch();
+}
