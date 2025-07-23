233b0edb129fbefdd7ca7607b228b457d10b64cc
romit2007 <romitvaghela2007@gmail.com>
2025-07-23 19:19:12 +0530

    Add files via upload

---
+++ 23-07-2025/task7.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    int num;
+    
+    printf("Enter num");
+    scanf("%d",&num);
+    
+    if(num%2==0)
+    {
+        printf("Even");
+    }
+    else
+    {
+        printf("odd");
+    }
+    return 0;
+}
