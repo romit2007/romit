d5cbb72f8cb13fbb7a37dcba16711cfe7405f538
romit2007 <romitvaghela2007@gmail.com>
2025-07-23 19:05:11 +0530

    Add files via upload

---
+++ 23-07-2025/task5.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    
+    float base,height,area;
+    
+    printf("Enter base");
+    scanf("%f",&base);
+    
+    printf("Enter height");
+    scanf("%f",&height);
+
+
+    area=0.5*base*height;
+    printf("\n area of triangle is %f",area);
+    return 0;
+}
