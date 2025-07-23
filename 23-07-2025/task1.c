6e02626b1227865b2fab74f3203276db2582b1d7
romit2007 <romitvaghela2007@gmail.com>
2025-07-23 18:45:05 +0530

    Add files via upload

---
+++ 23-07-2025/task1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    printf("\n Name : Romit");
+    printf("\n Birth date : 18-08-2007");
+    printf("\n Age: 18 ");
+    printf("\n Address: paramanand golden park-1 near 150 ft ring road rajkot");
+
+    return 0;
+}
