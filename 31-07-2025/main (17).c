3240b0ffcdfc7c9e7f5a1009858ae9400234dd70
romit2007 <romitvaghela2007@gmail.com>
2025-07-31 18:59:32 +0530

    Add files via upload

---
+++ 31-07-2025/main (17).c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int add(int a,int b)
+{
+    return a+b;
+}
+
+int main()
+{
+    printf("%d",add(6,3));
+     printf("%d",add(4,6));
+      printf("%d",add(8,3));
+       printf("%d",add(9,3));
+
+    return 0;
+}
