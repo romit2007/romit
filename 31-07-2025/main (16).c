938b84623b041a4dd09fc0c899227fda9a4bcf11
romit2007 <romitvaghela2007@gmail.com>
2025-07-31 18:46:09 +0530

    Add files via upload

---
+++ 31-07-2025/main (16).c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int x = 5;
+    x++;
+    printf("\n %d",x);
+    ++x;
+    printf("\n %d",x);
+    
+    
+    int y = 10;
+    
+    
+    int a = ++y;
+    printf("\n %d",a);
+    
+
+    return 0;
+}
