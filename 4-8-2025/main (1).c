c0a36499f1c6af5f73aac0b9a2edb1e1ae7f8ff5
romit2007 <romitvaghela2007@gmail.com>
2025-08-03 21:55:27 -0700

    Add files via upload

---
+++ 4-8-2025/main (1).c
@@ -0,0 +1,26 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+    int num1,num2;
+    
+    printf("Enter num1");
+    scanf("%d",&num1);
+    
+    printf("Enter num2");
+    scanf("%d",&num2);
+    
+    if(num1>num2)
+    {
+        printf("num1 is big");
+    }
+    else
+    {
+        printf("num2 is big");
+    }
+    
+
+    return 0;
+}
