0fa73141b95cc2198993f2d6f0d97b3d71d7bbab
romit2007 <romitvaghela2007@gmail.com>
2025-08-12 18:40:14 +0530

    Add files via upload

---
+++ 12-08-2025/main (20).c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    
+    
+    int a=5;
+    int b=10;
+    
+    if(a==5 && b==2)
+    {
+        printf("\n Condition is true");
+    }
+    else
+    {
+        printf("\n Condition isn't true");
+    }
+    
+     if(a==5 || b==2)
+    {
+        printf("\n Condition is true");
+    }
+    else
+    {
+        printf("\n Condition isn't true");
+    }
+    
+    
+    
+    if(!(a==5 || b==2))
+    {
+        printf("\n Condition is true");
+    }
+    else
+    {
+        printf("\n Condition isn't true");
+    }
+    
+    
+}
