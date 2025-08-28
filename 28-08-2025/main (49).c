9804e554105981c1a3a708a6f027f1e42f3a3bc9
romit2007 <romitvaghela2007@gmail.com>
2025-08-28 15:51:18 +0530

    Add files via upload

---
+++ 28-08-2025/main (49).c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int num,i,j;
+    
+    printf("Enter num");
+    scanf("%d",&num);
+    
+    for (i = 1; i <=num; i++) 
+    {
+
+        for (j = 1; j <= i; j++) 
+        {
+            printf("%d",num%2);
+            
+        }
+        printf("\n");
+    }
+    return 0;
+}
