540b18a16d771df946bfb69b32f25f7db36627cb
romit2007 <romitvaghela2007@gmail.com>
2025-09-03 22:00:59 +0530

    Add files via upload

---
+++ 3-9-2025/main (5).cpp
@@ -0,0 +1,34 @@
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    
+    int num1,num2,ans;
+    
+    cout<<"\n Enter number1: ";
+    cin>>num1;
+    
+    cout<<"\n Enter number2: ";
+    cin>>num2;
+    
+    ans=num1+num2;
+    cout<<"\n Answer : "<<ans;
+    
+    ans=num1*num2;
+    cout<<"\n Answer : "<<ans;
+    
+    ans=num1-num2;
+    cout<<"\n Answer : "<<ans;
+    
+    ans=num1/num2;
+    cout<<"\n Answer : "<<ans;
+    
+    
+    
+    
+    
+
+    return 0;
+}
