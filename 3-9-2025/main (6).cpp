10059dfa79c362a561e9f137ac0377a15cede8ba
romit2007 <romitvaghela2007@gmail.com>
2025-09-03 22:04:47 +0530

    Add files via upload

---
+++ 3-9-2025/main (6).cpp
@@ -0,0 +1,25 @@
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    
+    int num,i;
+    
+    
+    
+    for(i=1;i<=10;i++)
+    {
+        cout<<i<<endl;
+    }
+    
+    
+    
+    
+    
+    
+    
+
+    return 0;
+}
