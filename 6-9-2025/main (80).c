0533ce9515738f2fb5bdc4c703d73bf41c4d9caf
romit2007 <romitvaghela2007@gmail.com>
2025-09-06 10:30:29 +0530

    Add files via upload

---
+++ 6-9-2025/main (80).c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+int main()
+{
+    char ch[50];
+    
+    printf("Enter Characters: ");
+    scanf("%s",&ch);
+    
+    
+    printf("%d",strlen(ch));
+    
+    
+    
+    return 0;
+}
