3bff89324bd218fbe9be92d226fa8c5510bef433
romit2007 <romitvaghela2007@gmail.com>
2025-09-06 10:13:12 +0530

    Add files via upload

---
+++ 6-9-2025/main (79).c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+
+//with para with return type
+int cal(int a,int b)
+{
+    return a+b;
+}
+
+int main()
+{
+    printf("\n %d",cal(4,5));
+    printf("\n %d",cal(5,5));
+    printf("\n %d",cal(6,5));
+    printf("\n %d",cal(7,5));
+    printf("\n %d",cal(8,5));
+
+    return 0;
+}
