0a39560bfbe1de6ba7ffb4fdb6fb208a4d47bbc4
romit2007 <romitvaghela2007@gmail.com>
2025-09-08 19:31:36 +0530

    Add files via upload

---
+++ 8-9-2025/main (23).cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main()
+{
+    char name[50];
+    
+    
+    cout<<"Enter  Name";
+    cin>>name;
+    
+    
+    
+    cout<<strlen(name)<<endl;
+    
+    
+    
+
+    return 0;
+}
