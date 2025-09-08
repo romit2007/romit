bf9655ecb13533d8d6f804146fba7e6f9308e2e5
romit2007 <romitvaghela2007@gmail.com>
2025-09-08 19:04:58 +0530

    Add files via upload

---
+++ 8-9-2025/main (19).cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+using namespace std;
+
+//with para without return type
+int cal()
+{
+    int a=8;
+    int b=16;
+    
+    int c=a+b;
+    return c;
+    
+}
+
+int main()
+{
+    
+      cout<<"Addition of two value is: "<<cal();
+    
+
+    return 0;
+}
