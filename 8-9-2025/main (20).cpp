14a5c52a4c1ff2f3ecbfc1ca40ec372b1ac2a3a6
romit2007 <romitvaghela2007@gmail.com>
2025-09-08 19:06:59 +0530

    Add files via upload

---
+++ 8-9-2025/main (20).cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+using namespace std;
+
+
+void cal()
+{
+    int a=8;
+    int b=16;
+    
+    int c=a+b;
+    cout<<c;
+    
+}
+
+int main()
+{
+    
+     cal();
+    
+
+    return 0;
+}
