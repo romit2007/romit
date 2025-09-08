1298650d11e0842142c2f1a80c277014b441ce05
romit2007 <romitvaghela2007@gmail.com>
2025-09-08 19:35:41 +0530

    Add files via upload

---
+++ 8-9-2025/main (25).cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main()
+{
+    
+    string s1 = "Hello";
+    char ch[]={'t','o','p','s'};
+    
+     cout<<s1<<endl;
+    cout<<ch<<endl;
+
+    return 0;
+}
