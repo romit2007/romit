a4e7600ac510e9272d8d23a40c465e9e3c29cbf4
romit2007 <romitvaghela2007@gmail.com>
2025-09-29 19:41:22 +0530

    Add files via upload

---
+++ seven/model (1).sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 29, 2025 at 04:10 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `iphone`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `model`
+--
+
+CREATE TABLE `model` (
+  `id` int(11) NOT NULL,
+  `name` varchar(30) DEFAULT NULL,
+  `PRICE` bigint(190) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `model`
+--
+
+INSERT INTO `model` (`id`, `name`, `PRICE`) VALUES
+(1, 'iphone 11pro max', 45000),
+(2, 'iphone 12 pro max', 60000),
+(3, 'iphone 13pro max', 78000),
+(4, 'iphone 14 pro max', 80000),
+(5, 'iphone 15 pro max', 90000),
+(6, 'iphone 16 pro max', 120000),
+(7, 'iphone 17 air', NULL),
+(8, 'iphone 17 pro max', NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `model`
+--
+ALTER TABLE `model`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `model`
+--
+ALTER TABLE `model`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
